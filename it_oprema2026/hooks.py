e8936996be7ee65a3451ba810e7171141276d78b
sp9leo <Sp9leonardo@gmail.com>
2026-07-12 09:35:20 +0200

    Add app icon, sidebar with all doctypes, and workspace config
    - Add SVG logo and icons sprite to public/
    - Configure add_to_apps_screen and app_include_icons hooks
    - Add permission api module
    - Populate workspace_sidebar with all 17 doctypes
    - Update workspace reservations.json with shortcuts and links
    - Fix app field in workspace_sidebar to it_oprema2026
    - Fix module field to match modules.txt

--- it_oprema2026/hooks.py
+++ it_oprema2026/hooks.py
@@ -1,11 +1,21 @@
 app_name = "it_oprema2026"
 app_title = "IT Oprema 2026"
-app_publisher = "OS\u0160"
+app_publisher = "OS\u017d"
 app_description = "IT asset management and device booking"
 app_email = "info@osaz.si"
 app_license = "MIT"
 
 app_include_js = "/assets/it_oprema2026/js/it_oprema.js"
+app_include_icons = "/assets/it_oprema2026/icons/icons.svg"
+
+add_to_apps_screen = [
+    {
+        "name": "it_oprema2026",
+        "logo": "/assets/it_oprema2026/images/logo.svg",
+        "title": "IT Oprema 2026",
+        "route": "/it_oprema2026",
+    }
+]
 
 scheduler_events = {
     "daily": [
